e4d19a0f77a2d76cea0226ff582feafcb0f476d4
robertrb63 <81638977+robertrb63@users.noreply.github.com>
2023-05-11 14:21:38 -0500

    Update crud.py

--- crud.py
+++ crud.py
@@ -67,7 +67,20 @@ async def user(user: User):
         return {"error":"El usuario ya existe"}
     else:
         users_list.append(user)
+ 
+@app.put("/user/")
+async def user(user: User):
+    found = False
     
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == user.id:
+            users_list[index] = user
+            found = True
+    if not found:
+        return {"error":"No se ha actualizado el usuario"}    
+    else:
+        return user
+
 def search_user(id: int):
     users = filter(lambda user: user.id == id, users_list)
     try:
