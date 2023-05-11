33051ccbd92dab4d0fc33a576dbeff5e685235e8
robertrb63 <81638977+robertrb63@users.noreply.github.com>
2023-05-11 13:09:57 -0500

    Update users.py

--- users.py
+++ users.py
@@ -32,25 +32,45 @@ async def users():
     return users_list
     #return User(name="Rodrigo",surname="Marin",email="rm@gmail.com",age=45)
     
+@app.get("/users")
+async def users():
+    return users_list
+    #return User(name="Rodrigo",surname="Marin",email="rm@gmail.com",age=45)
+    
+# iniciar navegador con:  http://127.0.0.1:8000/users
+
 @app.get("/user/{id}")
 async def user(id:int):
     users = filter(lambda user: user.id == id, users_list)
-    return list(users)[0] @app.get("/user/{id}")
-async def user(id:int):
-    return users_list   
+    try:
+        return list(users)[0]  
+    except:
+        return {"error":"No se ha encontrado el usuario"}
+# iniciar navegador con:  http://127.0.0.1:8000/user/1    
 
+
+@app.get("/username/")
+async def username(name:str):
+    users = filter(lambda user: user.name == name, users_list)
+    try:
+        return list(users)[0]  
+    except:
+        return {"error":"No se ha encontrado el usuario"}
+# iniciar navegador con:  http://127.0.0.1:8000/username/?name=Roberto   
+    
 @app.get("/user/")
 async def user(id:int):
     return search_user(id)
     
     # iniciar navegador con:  http://127.0.0.1:8000/userquery/?id=1
-    
+   
+    # iniciar navegador con:  http://127.0.0.1:8000/username/?name=roberto   
+
 
 def search_user(id: int):
     users = filter(lambda user: user.id == id, users_list)
     try:
         return list(users)[0]  
     except:
-        return {"error":"No se ha encontrado el usuario"}
-    
-    # iniciar navegador con:  http://127.0.0.1:8000/userquery/?id=1 
+        return {"error":"No se ha encontrado el usuario"}   
+    # iniciar navegador con:  http://127.0.0.1:8000/userquery/?id=1
